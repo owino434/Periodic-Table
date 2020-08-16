9121634a45be5f3cad2390fca262ad904f81b228
owino434 <69758150+owino434@users.noreply.github.com>
2020-08-16 17:04:15 +0300

    Add files via upload

---
+++ elements.sql
@@ -0,0 +1,13 @@
+INSERT INTO element VALUES( NULL,'Beryllium', 4, 'Be', 2, '2.2', 'high', 9);
+INSERT INTO element VALUES( NULL,'Magnessium', 12, 'Mg', 2, '2.8.2', 'high', 24);
+INSERT INTO element VALUES( NULL,'Calcium', 20, 'Ca', 2, '2.8.8.2', 'high', 40);
+INSERT INTO element VALUES( NULL,'Strontium', 38, 'Sr', 2, '2.8.8.8.2', 'high',87);
+INSERT INTO element VALUES( NULL,'Barium', 56, 'Ba', 2, '2.8.8.8.2', 'high',137);
+INSERT INTO element VALUES( NULL,'Radium', 88, 'Ra', 2, '2.8.8.8.2', 'high',226);
+
+
+
+
+INSERT INTO element VALUES( NULL,'Radium', 88, 'Ra', 2, '2.8.8.8.2', 'high',226);
+
+
---
+++ transition.sql
@@ -0,0 +1,43 @@
+INSERT INTO element VALUES( NULL,'Scandium',	21,	'Sc'	,'Transition'	,'0'	,'high',	44);
+INSERT INTO element VALUES( NULL,'Titanium',	22,	'Ti'	,'Transition'	,'0'	,'high',	48);
+INSERT INTO element VALUES( NULL,'Vanadium',	23,	'V'	,'Transition'	,'0'	,'high',	51);
+INSERT INTO element VALUES( NULL,'Chromium',	24,	'Cr'	,'Transition'	,'0'	,'high',	52);
+INSERT INTO element VALUES( NULL,'Manganese',	25,	'Mn'	,'Transition'	,'0'	,'high',	55);
+INSERT INTO element VALUES( NULL,'Cobalt',	27,	'Co'	,'Transition'	,'0'	,'high',	58.9);
+INSERT INTO element VALUES( NULL,'Nickel',	28,	'Ni'	,'Transition'	,'0'	,'high',	58.7);
+INSERT INTO element VALUES( NULL,'Copper',	29,	'Cu'	,'Transition'	,'0'	,'high',	64);
+INSERT INTO element VALUES( NULL,'Zinc',	30,	'Zn'	,'Transition'	,'0'	,'high',	65);
+
+INSERT INTO element VALUES( NULL,'Yttrium',	39,	'Y'	,'Transition'	,'0'	,'high',	89);
+INSERT INTO element VALUES( NULL,'Zirconium',	40,	'Zr'	,'Transition'	,'0'	,'high',	91);
+INSERT INTO element VALUES( NULL,'Niobium',	41,	'Nb'	,'Transition'	,'0'	,'high',	93);
+INSERT INTO element VALUES( NULL,'Mollybednum',	42,	'Mo'	,'Transition'	,'0'	,'high',	96);
+INSERT INTO element VALUES( NULL,'Technetium',	43,	'Tc'	,'Transition'	,'0'	,'high',	98);
+INSERT INTO element VALUES( NULL,'Ruthenium',	44,	'Ru'	,'Transition'	,'0'	,'high',	101);
+INSERT INTO element VALUES( NULL,'Rhodium',	45,	'Rh'	,'Transition'	,'0'	,'high',	103);
+INSERT INTO element VALUES( NULL,'Palladium',	46,	'Pd'	,'Transition'	,'0'	,'high',	106);
+INSERT INTO element VALUES( NULL,'Silver',	47,	'Ag'	,'Transition'	,'0'	,'high',	108);
+INSERT INTO element VALUES( NULL,'Cadmium',	48,	'Cd'	,'Transition'	,'0'	,'high',	112);
+
+
+
+
+INSERT INTO element VALUES( NULL,'Hafnium',	72,	'Hf'	,'Transition'	,'0'	,'high',	181);
+INSERT INTO element VALUES( NULL,'Tantalum',	73,	'Ta'	,'Transition'	,'0'	,'high',	181);
+INSERT INTO element VALUES( NULL,'Tungsten',	74,	'W'	,'Transition'	,'0'	,'high',	184);
+INSERT INTO element VALUES( NULL,'Rhenium',	75,	'Re'	,'Transition'	,'0'	,'high',	186);
+INSERT INTO element VALUES( NULL,'Osmium',	76,	'Os'	,'Transition'	,'0'	,'high',	190);
+INSERT INTO element VALUES( NULL,'Iridium',	77,	'Ir'	,'Transition'	,'0'	,'high',	192);
+INSERT INTO element VALUES( NULL,'Platinum',	78,	'Pt'	,'Transition'	,'0'	,'high',	195);
+INSERT INTO element VALUES( NULL,'Gold',	79,	'Ag'	,'Transition'	,'0'	,'high',	197);
+INSERT INTO element VALUES( NULL,'Mercury',	80,	'Hg'	,'Transition'	,'0'	,'high',	201);
+
+INSERT INTO element VALUES( NULL,'Rutherfordium',	104,	'Rf'	,'Transition'	,'0'	,'high',	261);
+INSERT INTO element VALUES( NULL,'Dubnium',	105,	'Db'	,'Transition'	,'0'	,'high',	262);
+INSERT INTO element VALUES( NULL,'Seaborgium',	106,	'Sg'	,'Transition'	,'0'	,'high',	263);
+INSERT INTO element VALUES( NULL,'Bohrium',	107,	'Bh'	,'Transition'	,'0'	,'high',	262);
+INSERT INTO element VALUES( NULL,'Hassium',	108,	'Hs'	,'Transition'	,'0'	,'high',	265);
+INSERT INTO element VALUES( NULL,'Meitnerium',	109,	'Mt'	,'Transition'	,'0'	,'high',	266);
+INSERT INTO element VALUES( NULL,'Ununnillum',	110,	'Uum'	,'Transition'	,'0'	,'high',	269);
+INSERT INTO element VALUES( NULL,'Unununium',	111,	'Uuu'	,'Transition'	,'0'	,'high',	272);
+INSERT INTO element VALUES( NULL,'Ununbium',	112,	'Uub'	,'Transition'	,'0'	,'high',	272);
